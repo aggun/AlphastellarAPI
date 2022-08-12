37ca07911da438752e94779be8f4803875aa2cc4
Nebil Aggun <nebil.aggun@gmail.com>
2022-08-12 03:21:31 +0300

    ServiceLayer Completed

---
+++ ServiceLayer/Mapping/MapProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CoreLayer.DTOs;
+using CoreLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+                CreateMap<Boat, BoatDto>().ReverseMap();
+                CreateMap<Bus, BusDto>().ReverseMap();
+                CreateMap<Car, CarDto>().ReverseMap();
+                CreateMap<Car, CarUpdateDto>().ReverseMap();
+        }
+        }
+    }
---
+++ ServiceLayer/Services/Service.cs
@@ -0,0 +1,45 @@
+﻿using CoreLayer.Repositories;
+using CoreLayer.Services;
+using CoreLayer.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+        private readonly IUnitOfWork _unitOfWork;
+        public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task RemoveAsync(T entity)
+        {
+            _repository.Remove(entity);
+            await _unitOfWork.CommitAsync();
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var hasProduct = await _repository.GetByIdAsync(id);
+
+            return hasProduct;
+        }
+        public async Task UpdateAsync(T entity)
+        {
+            _repository.Update(entity);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _repository.where(expression);
+        }
+
+    }
+}
