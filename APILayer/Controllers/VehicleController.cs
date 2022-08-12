b87e66cd8ccc09d0f23dac26a2aafbea718b7544
Nebil Aggun <nebil.aggun@gmail.com>
2022-08-12 03:20:12 +0300

    API Layer Completed

---
+++ APILayer/Controllers/VehicleController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using CoreLayer.DTOs;
+using CoreLayer.Models;
+using CoreLayer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APILayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Car> _carService;
+        private readonly IService<Bus> _busService;
+        private readonly IService<Boat> _boatService;
+
+        public VehicleController(IMapper mapper, IService<Car> carService, IService<Bus> busService , IService<Boat> boatService )
+        {
+            _mapper = mapper;
+            _carService = carService;
+            _busService = busService;
+            _boatService = boatService;
+        }
+        [HttpGet("GetCarByColor/{id}")]
+        public IActionResult GetCarByColor(int id)
+        {
+            var cars = _carService.Where(x => x.ColorId == id);
+            var carsDtos = _mapper.Map<List<CarDto>>(cars.ToList());
+            return Ok(carsDtos);
+        }
+        [HttpGet("GetBusByColor/{id}")]
+        public IActionResult GetBusByColor(int id)
+        {
+            var buses = _busService.Where(x => x.ColorId == id);
+            var busesDtos = _mapper.Map<List<BusDto>>(buses.ToList());
+            return Ok(busesDtos);
+        }
+        [HttpGet("GetBoatByColor/{id}")]
+        public IActionResult GetBoatByColor(int id)
+        {
+            var boats = _boatService.Where(x => x.ColorId == id);
+            var boatsDtos = _mapper.Map<List<BoatDto>>(boats.ToList());
+            return Ok(boatsDtos);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(CarUpdateDto carUpdateDto)
+        {
+            await _carService.UpdateAsync(_mapper.Map<Car>(carUpdateDto));
+            return Ok(204);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var car = await _carService.GetByIdAsync(id);
+            await _carService.RemoveAsync(car);
+            return Ok(204);
+        }
+    }
+}
--- APILayer/Program.cs
+++ APILayer/Program.cs
@@ -1,5 +1,9 @@
+using APILayer.Modules;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using RepositoryLayer;
+using ServiceLayer.Mapping;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +15,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAutoMapper(typeof(MapProfile));
+
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
     x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
@@ -18,6 +24,10 @@ builder.Services.AddDbContext<AppDbContext>(x =>
         option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
     });
 });
+
+builder.Host.UseServiceProviderFactory
+    (new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
 
 var app = builder.Build();
 
